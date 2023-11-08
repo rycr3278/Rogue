1df1952a5244bc762ac1425639595dbaadc27c5c
Ryan <134633241+rycr3278@users.noreply.github.com>
2023-11-07 21:19:55 -0500

    Delete config directory

--- config/globals.py
+++
@@ -1,9 +0,0 @@
-# globals.py
-import pygame as py
-
-global enemy_group, item_group
-
-enemy_group = py.sprite.Group()
-item_group = py.sprite.Group()
-
-# ... Add other global game-wide variables as needed
